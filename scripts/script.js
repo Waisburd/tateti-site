23b07dd2254083f091a2b0a9248692292b373ffb
Tomas Waisburd <tomaswa@lagash.com>
2019-06-06 20:08:37 -0300

    se agregan resources y files

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.square').click(function(event){
+        console.log('element touched: ' + event.target.id);
+
+        // $('#' + event.target.id).backgroundImage = "url('../resources/illuminati_white.png')";
+        // $('#' + event.target.id).attr('backgroundImage', '../resources/illuminati_white.png');
+
+    });
+
+
+
+    // .append($("<p class='ficha'>X</p>"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
+
